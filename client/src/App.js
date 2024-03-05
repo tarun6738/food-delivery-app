af635a6c857df7b99534bece52bed6992db40da5
tarun6738 <tarunnemali2004@gmail.com>
2024-03-05 22:43:54 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import {
 import Home from './screens/Home';
 import Login from './screens/Login';
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import Signup from './screens/Signup';
