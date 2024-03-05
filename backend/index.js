48ddfa093aa3a0a09f89b54b07e2165cb995ea32
tarun6738 <118709508+tarun6738@users.noreply.github.com>
2024-03-05 12:47:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,14 @@ const app = express();
 const mongoDB = require('./db');
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: [""],
+        methods:["POST","GET"],
+        credentials: true
+]
+    }
+))
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header(
